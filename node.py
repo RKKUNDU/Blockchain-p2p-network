dc88921ab88eab3361079e2fa76e82ea4d8c7f8d
Pramod S <pramodrao@cse.iitb.ac.in>
2020-08-31 00:31:15 +0530

    updated to send/recv simple messages between nodes

--- node.py
+++ node.py
@@ -4,27 +4,34 @@ import time
 import socket               
 import threading
 
-def receive_message():
-    
-    host = socket.gethostname() 
-    port = 12345                
-    s = socket.socket()         
-    s.bind((host, port))        
-    s.listen(5) 
-    c, addr = s.accept()
-    print('Got connection from', addr)  
-    while True:
-        print(c.recv(1024))   
+HOST = '192.168.0.182'
+PORT = 27777
 
+CONNECT_TO = '192.168.56.102'
+
+def receive_message():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                print(data.decode('utf-8'))
+                if not data:
+                    break
 
 def send_message():
-    host = socket.gethostname() 
-    port = 12345
-    s = socket.socket()
-    s.connect((host, port))
-    while True:
-        time.sleep(5)
-        s.send("Sending this every 5 seconds as a sender")
+    time.sleep(7)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((CONNECT_TO, PORT))
+        while True:
+            inp = input()
+            if inp=='exit':
+                return
+            s.sendall(inp.encode('utf-8'))
+        
 
 t1 = threading.Thread(target=receive_message, name='t1') 
 t2 = threading.Thread(target=send_message, name='t2')   
@@ -36,11 +43,3 @@ t2.start()
 # wait until all threads finish 
 t1.join() 
 t2.join() 
-
-        
-    
-
-
-
-
-
