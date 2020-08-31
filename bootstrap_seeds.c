188d54f68687dc2d18f74f2b5bc29b7da42d6547
Sumit Thorat <sumit.thorat97@gmail.com>
2020-08-31 23:03:36 +0530

    Add code to bootstrap multiple seeds with supplied port number from config.csv

---
+++ bootstrap_seeds.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LEN 1024
+#define MAX_TOKENS 64
+
+char** split(char*, char*);
+int is_delim(char, char*);
+void run(char*);
+
+int main(int argc, char** argv) {
+	FILE* fp = fopen("config.csv", "rw");
+    char* buffer = (char*) malloc(MAX_LEN * sizeof(char));
+	size_t t;
+	int len;
+	int count = 0;
+	char** lines = (char**) malloc(MAX_TOKENS * sizeof(char*));
+	while ((len = getline(&buffer, &t, fp)) > 0) {
+		if (buffer[len - 1] == '\n')
+			buffer[len - 1] = '\0';
+		char* line = (char*) malloc(MAX_LEN * sizeof(char));
+		strcpy(line, buffer);
+		lines[count++] = line;
+	}
+	free(buffer);
+
+
+
+    for (int i = 0; i < count; ++i) {
+        char** tokens = split(lines[i], ",");
+
+        int id = fork();
+
+        if (id == 0) {
+            run(tokens[1]);
+        }
+    }
+}
+
+void run(char* port) {
+    char* args[4] = {"python3", "server.py", port, NULL};
+    execvp(args[0], args);
+}
+
+char** split (char* str, char* delims) {
+	char** tokens = (char**) malloc (MAX_TOKENS * sizeof(char*));
+	char* token = (char*) malloc(MAX_LEN * sizeof(char));
+	int len = (int) strlen(str), idx = 0, t_no = 0;
+
+	if (len == 0) {
+		tokens[0] = NULL;
+		free(token);
+		return tokens;
+	}
+
+	// Break string
+	for (int i = 0; i <= len; ++i) {
+		char c = str[i];
+
+		// if (i > 0 && is_delim(str[i - 1], delims) && is_delim(c, delims)) continue;
+
+		if (is_delim(c, delims) || c == '\0') {
+			token[idx] = '\0';
+			tokens[t_no++] = token;
+			idx = 0;
+			token = (char*) malloc(MAX_LEN * sizeof(char));
+		} else {
+			token[idx++] = c;
+		}
+	}
+
+	tokens[t_no] = NULL;
+	free(token);
+	return tokens;
+}
+
+int is_delim(char c, char* delims) {
+	for (int i = 0; i < strlen(delims); ++i) {
+		if (c == delims[i])
+			return 1;
+	}
+	return 0;
+}
