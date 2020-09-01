865bdcb46497481e34b693bb17906923eb388e39
Pramod S <pramodrao@cse.iitb.ac.in>
2020-09-01 11:38:23 +0530

    updated seed to have a thread per client

--- seed.py
+++ seed.py
@@ -4,14 +4,23 @@ import time
 import socket               
 import threading
 
-peer_list=[]
+peer_list=set()
 
 def register_request(socket_pair):
-    peer_list.append(socket_pair)
+    peer_list.add(socket_pair)
 
 #Just a placeholder for now, will implement later
 def dead_node_message():
     pass
+
+def new_client(conn):
+    data=conn.recv(1024)
+    print(data.decode('utf-8'))
+    register_request(eval(data.decode('utf-8')))
+    conn.sendall(str(peer_list).encode('utf-8'))
+    while True:
+        data=conn.recv(1024)
+        conn.sendall(data)
 
 def start_seed_node():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -19,9 +28,8 @@ def start_seed_node():
         s.listen()
         while True:
             conn, (ip,port) = s.accept()
-            data=conn.recv(1024)
-            register_request(eval(data.decode('utf-8')))
-            conn.sendall(str(peer_list).encode('utf-8'))
+            pthread=threading.Thread(target=new_client,args=[conn])
+            pthread.start()
 
 t1 = threading.Thread(target=start_seed_node, name='t1') 
 
