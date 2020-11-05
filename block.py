c755f7182fef41ad579a693bb3d05ec5d5a225fb
RKKUNDU <kundu.rohit06@gmail.com>
2020-11-05 13:15:57 +0530

    Add a way to initialize a block with only block string in Block class

--- block.py
+++ block.py
@@ -1,9 +1,18 @@
+PREV_HASH_FIELD = 4
+MERKEL_ROOT_FIELD = 2
+TIMESTAMP_FIELD = 10
 class Block:
-
     def __init__(self, prev_hash, merkel_root, timestamp):
         self.prev_hash = prev_hash # 4 characters
         self.merkel_root = merkel_root # 2 characters
         self.timestamp = timestamp # 10 characters
+
+    @classmethod
+    def set_block(self, block_string):
+        prev_hash = block_string[:PREV_HASH_FIELD] # first 4 byte is prevHash hex digest
+        merkel_root = block_string[PREV_HASH_FIELD:PREV_HASH_FIELD + MERKEL_ROOT_FIELD]
+        timestamp = block_string[-TIMESTAMP_FIELD:]
+        return Block(prev_hash, merkel_root, timestamp)
 
     def __str__(self):
         return f'{self.prev_hash}{self.merkel_root}{self.timestamp}'
@@ -11,7 +20,7 @@ class Block:
     def get_prev_block_hash(self):
         return self.prev_hash
 
-    def get_merket_root(self):
+    def get_merkel_root(self):
         return self.merkel_root
 
     def get_timestamp(self):
